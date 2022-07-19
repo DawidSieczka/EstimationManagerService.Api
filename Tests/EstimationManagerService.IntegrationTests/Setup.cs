fc5ab8c895010d50b58e67bd72d72e498912aa9d
DawidSieczka <dawid.sieczka.praca@gmail.com>
2022-07-19 23:10:46 +0200

    Added empty tests

---
+++ Tests/EstimationManagerService.IntegrationTests/Setup.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace EstimationManagerService.IntegrationTests;
+
+[SetUpFixture]
+public class Tests
+{
+    [OneTimeSetUp]
+    public void Setup()
+    {
+    }
+}
