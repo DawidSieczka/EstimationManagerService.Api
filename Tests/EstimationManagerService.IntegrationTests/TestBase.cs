4d2bb8be6e1c590a2e81d3fa2deb961733a80814
DawidSieczka <dawid.sieczka.praca@gmail.com>
2022-07-20 00:16:49 +0200

    Added tests setup with sqldb

---
+++ Tests/EstimationManagerService.IntegrationTests/TestBase.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace EstimationManagerService.IntegrationTests;
+
+using static SetupTests;
+
+public class TestBase
+{
+    [SetUp]
+    public async Task SetUp()
+    {
+        await ResetState();
+    }
+}
---
+++ Tests/EstimationManagerService.IntegrationTests/Tests/Operations/Users/Commands/CreateUserTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstimationManagerService.IntegrationTests.Tests.Operations.Users.Commands;
+
+public class CreateUserTests
+{
+    
+}
