13f99517700a0419d2e281013b86ef7d735ff02d
DawidSieczka <dawid.sieczka.praca@gmail.com>
2022-07-19 23:34:46 +0200

    separated startup from program for tests

--- EstimationManagerService.Api/Extensions/DbSetupExtensions.cs
+++ EstimationManagerService.Api/Extensions/DbSetupExtensions.cs
@@ -5,14 +5,15 @@ namespace EstimationManagerService.Api.Extensions;
 
 public static class DbSetupExtensions
 {
-    public async static Task SetupDatabase(this IServiceCollection services, string sqlDbConnectionString)
+    public static void SetupDatabase(this IServiceCollection services, string sqlDbConnectionString)
     {
         services.AddDbContext<AppDbContext>(b => b
             .UseLazyLoadingProxies()
             .UseSqlServer(sqlDbConnectionString, options => options.CommandTimeout(120)
+            .EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)
         ));
 
         using var appDbContext = services.BuildServiceProvider().GetRequiredService<AppDbContext>();
-        await appDbContext.Database.MigrateAsync();
+        appDbContext.Database.Migrate();
     }
 }
--- EstimationManagerService.Api/Program.cs
+++ EstimationManagerService.Api/Program.cs
@@ -1,60 +1,16 @@
-using EstimationManagerService.Api.Extensions;
-using EstimationManagerService.Application.Operations.UserTasks.Queries.GetUserTasks;
-using MediatR;
-using System.Reflection;
-using EstimationManagerService.Application.Common.Exceptions;
-using System.Text.Json.Serialization;
-using FluentValidation.AspNetCore;
-using EstimationManagerService.Application.Operations.Users.Commands.CreateUser;
-using EstimationManagerService.Application.Common.Options;
-using Microsoft.Extensions.Options;
+using EstimationManagerService.Api;
+
 internal class Program
 {
     private static async Task Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var config = builder.Configuration;
-
-        builder.Services.AddControllers(options =>
-        {
-            options.Filters.Add(new HttpResponseExceptionFilter(builder.Environment));
-        })
-        .AddJsonOptions(options =>
-        {
-            options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-        })
-        .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateUserCommand>());
-
-        builder.Services.AddRouting(options => options.LowercaseUrls = true);
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.ConfigureSwaggerGen();
-        builder.Services.RegisterDependenciesInjections();
-        builder.Services.AddMediatR(Assembly.GetAssembly(typeof(GetUserTasksQuery)));
-        builder.Services.AddConfigurationOptions(config);
-
-        var connectionStrings = builder.Services.BuildServiceProvider().GetRequiredService<IOptions<ConnectionStringsOptions>>().Value;
-        await builder.Services.SetupDatabase(connectionStrings.SqlDatabase);
-
-        var app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ContactReplicationService.Api v1");
-                c.RoutePrefix = string.Empty;
-            });
-        }
-
-        app.ConfigureExceptionHandler(app.Environment);
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+        await CreateHostBuilder(args).Build().RunAsync();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+               .ConfigureWebHostDefaults(webBuilder =>
+               {
+                   webBuilder.UseStartup<Startup>();
+               });
 }
